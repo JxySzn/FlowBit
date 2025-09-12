83939ba8d2fd441a29758163116478f8764b1bca
Jason <byronjason902@gmail.com>
2025-09-12 17:40:21 +0100

    feat: Add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Header } from "@/sections/Header";
+import { Hero } from "@/sections/Hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <h1>Hello world</h1>
+      <Hero />
     </>
   );
 }
