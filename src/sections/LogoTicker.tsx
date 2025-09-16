da987ef62fe604da2b44f0ec6e535614120053a3
Jason <byronjason902@gmail.com>
2025-09-16 10:55:38 +0100

    feat: Add Product showcase section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,30 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import apexLogo from "@/assets/logo-apex.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import Image from "next/image";
+
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="py-8 md:py-12 bg-white">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={acmeLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image src={quantumLogo} alt="quantum Logo" className="" />
+            <Image src={echoLogo} alt="echo Logo" className="" />
+            <Image src={celestialLogo} alt="celestial Logo" className="" />
+            <Image src={apexLogo} alt="apex Logo" className="" />
+            <Image src={pulseLogo} alt="pulse Logo" className="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
