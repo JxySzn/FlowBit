766a94d12a8b3c72bfd57fa9c20b163d503d8b61
Jason <byronjason902@gmail.com>
2025-09-23 22:08:40 +0100

    feat: Add Nav name

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,4 +1,4 @@
- import ArrowRight from "@/assets/arrow-right.svg";
+import ArrowRight from "@/assets/arrow-right.svg";
 import Logo from "@/assets/logosaas.png";
 import Image from "next/image";
 import MenuIcon from "@/assets/menu.svg";
@@ -7,7 +7,9 @@ export const Header = () => {
   return (
     <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
-        <p className="text-white/60 hidden md:block ">Streamline your work flow and boost your productivity</p>
+        <p className="text-white/60 hidden md:block ">
+          Streamline your work flow and boost your productivity
+        </p>
         <div className="inline-flex gap-1 items-center">
           <p>Get started for free</p>
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
@@ -18,7 +20,12 @@ export const Header = () => {
       <div className="py-5">
         <div className="container">
           <div className="flex items-center justify-between">
-            <Image src={Logo} alt="Saas Logo" height={40} width={40} />
+            <div className="flex items-center gap-3">
+              <Image src={Logo} alt="Saas Logo" height={40} width={40} />
+              <span className="hidden sm:inline-block text-base md:text-lg lg:text-2xl text-black">
+                FlowBit
+              </span>
+            </div>
             <MenuIcon className="h-5 w-5 md:hidden" />
             <nav className=" hidden md:flex gap-6 text-black/60 items-center">
               <a href="#">About</a>
